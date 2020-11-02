b608c5acfd9e557ed4e5dcc16d5484fbc23ef869
HasinRaihan223 <hraihan20@stuy.edu>
2020-11-02 17:39:02 -0500

    Added size, add, and get method and they work

--- SuperArray.java
+++ SuperArray.java
@@ -7,4 +7,30 @@ public class SuperArray{
     data = new String[10];
   }
 
+  public int size(){
+    return size;
+  }
+
+  public boolean add(String element){
+    data[size] = element;
+    size++;
+    return true;
+  }
+
+  public String get(int index){
+    return data[index];
+  }
+
+  /*
+  public static void main(String[]arg){
+    SuperArray words = new SuperArray();
+    words.add("kani");
+    words.add("uni");
+    words.add("ebi");
+    for(int i = 0; i < words.size(); i++){
+      System.out.println( words.get(i) );
+    }
+  }
+  */
+
 }
