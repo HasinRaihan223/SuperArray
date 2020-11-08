a6dee7eaccf64a923a52ea106208fcb5a051700b
HasinRaihan223 <hraihan20@stuy.edu>
2020-11-08 17:38:10 -0500

    Initialized demo file

---
+++ Demo.java
@@ -0,0 +1,24 @@
+public class Demo{
+
+  public static void removeDuplicates(SuperArray s){
+    for(int i = 0; i < s.size(); i++){
+      String temp = s.get(i)
+      for(int j = s.size()-1; j > i ; j--){
+        if (temp.equals(s.get(j))) s.remove(j);
+      }
+    }
+  }
+
+  public static void main(String[]args){
+    SuperArray words = new SuperArray();
+    //grouped to save vertical space
+    words.add("kani");   words.add("uni");     words.add("ebi");     words.add("una");
+    words.add("una");    words.add("ebi");     words.add("kani");    words.add("una");
+    words.add("una");    words.add("ebi");     words.add("toro");
+
+    System.out.println(words);
+    removeDuplicates(words);
+    System.out.println(words);
+  }
+
+}
